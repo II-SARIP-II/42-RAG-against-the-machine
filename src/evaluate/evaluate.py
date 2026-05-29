10887bc19b0702185daca75941eb0e1277bf4b99
II-SARIP-II <paris.gougne.05@gmail.com>
2026-05-29 17:32:23 +0200

    start evaluation

--- src/evaluate/evaluate.py
+++ src/evaluate/evaluate.py
@@ -14,7 +14,8 @@ class Evaluate():
     def get_result(self):
         """Load search results JSON."""
         try:
-            with (open(self.config.dataset_path, "r")as file):
+            print(self.config.dataset_path)
+            with open(self.config.dataset_path, "r", encoding="utf-8") as file:
                 data = file.read()
                 return StudentDetailedSearchResults(**json.loads(data))
         except Exception:
@@ -23,11 +24,25 @@ class Evaluate():
     def get_answered_questions(self):
         """Load labeled questions JSON."""
         try:
-            with (open(self.config.answer_path, "r")as file):
+            with open(self.config.answer_path, "r", encoding="utf-8") as file:
                 data = file.read()
                 return RagDataset(**json.loads(data))
         except Exception:
             raise (ValueError(f"Cannot read {self.config.answer_path}"))
 
     def calculate_recall(self):
-        print(self.searched_data, self.answers)
+        answers_dict = {answer.question_id: answer for answer in self.answers.rag_questions}
+        count = 0
+        for question in self.searched_data.search_results:
+            if question.question_id in answers_dict:
+
+                
+
+    def is_matching()
+        for qsource in question.retrieved_sources:
+            if qsource.file_path != answers_dict[question.question_id].sources.file_path:
+                continue
+            if not is_overlaped(qsource, answers_dict[question.question_id].sources):
+                continue
+            return True
+        return False
